d531eb100b055f1fd97c2f8aef183fa210e2be4d
Jujuxia <nameJujuxia@yeah.net>
2020-07-17 23:21:54 +0800

    [add]-init project

--- vuedraggable-demo/src/App.vue
+++ vuedraggable-demo/src/App.vue
@@ -1,28 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="cotainer mt-5">
+    <div class="row">
+      <div class="col form-inline">
+        <b-form-input v-model="newTask" required placeholder="回车添加任务" @keyup.enter="add"></b-form-input>
+        <b-button @click="add" variant="primary" class="ml-3">添加</b-button>
+      </div>
+    </div>
+    <div class="row mt-5">
+      <div class="col-3">
+        <div class="p-2 alert alert-secondary">
+          <h3>项目名称</h3>
+          <draggable class="list-group ms-column" group="tasks" :list="logs">
+            <div class="list-group-item" v-for="(element, index) in logs" :key="index">{{element.name}}</div>
+          </draggable>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="p-2 alert alert-primary">
+          <h3>项目进度</h3>
+          <draggable class="list-group ms-column" group="tasks" :list="progress">
+            <div class="list-group-item" v-for="(element, index) in progress" :key="index">{{element.name}}</div>
+          </draggable>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="p-2 alert alert-warning">
+          <h3>项目测试</h3>
+          <draggable class="list-group ms-column" group="tasks" :list="test">
+            <div class="list-group-item" v-for="(element, index) in test" :key="index">{{element.name}}</div>
+          </draggable>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="p-2 alert alert-success">
+          <h3>项目上线</h3>
+          <draggable class="list-group ms-column" group="tasks" :list="done">
+            <div class="list-group-item" v-for="(element, index) in done" :key="index">{{element.name}}</div>
+          </draggable>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import draggable from 'vuedraggable'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        newTask: "",
+        logs: [
+          {name: "vue-draggable-demo"},
+          {name: "拖拽功能"},
+          {name: "测试功能"},
+          {name: "打包上线"},
+        ],
+        progress: [],
+        test: [],
+        done: []
+      }
+    },
+    methods: {
+      add() {
+        if (this.newTask) {
+          this.logs.push({name: this.newTask});
+          this.newTask = "";
+        }
+      }
+    },
+    components: {
+      draggable
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .ms-column {
+    min-height: 300px;
+  }
 </style>
